614211084e81fdd880b022df9278925526f13465
michel <michel@randahl.net>
2024-05-16 16:37:54 +0200

    neovim: set theme and colours based on time of day

--- .config/nvim/after/plugin/colorschemes.lua
+++ .config/nvim/after/plugin/colorschemes.lua
@@ -5,8 +5,36 @@ vim.cmd [[
   hi NonText guibg=NONE ctermbg=NONE
 ]]
 
-vim.opt.background = "dark"
-vim.cmd('colorscheme monokai-nightasty')
+local current_time = os.date("*t")
+local eight_am = { hour = 8, min = 0, sec = 0 }
+local six_pm = { hour = 18, min = 0, sec = 0 }
+
+-- Function to compare two times
+local function is_after(time1, time2)
+  if time1.hour > time2.hour then
+    return true
+  elseif time1.hour == time2.hour then
+    if time1.min > time2.min then
+      return true
+    elseif time1.min == time2.min then
+      return time1.sec >= time2.sec
+    end
+  end
+  return false
+end
+
+-- Function to check if the current time is between two times
+local function is_between(time, start_time, end_time)
+  return is_after(time, start_time) and not is_after(time, end_time)
+end
+
+if is_between(current_time, eight_am, six_pm) then
+  vim.opt.background = "light"
+  vim.cmd('colorscheme daylight')
+else
+  vim.opt.background = "dark"
+  vim.cmd('colorscheme monokai-nightasty')
+end
 
 _G.switch_colorscheme = function()
   if(vim.opt.background._value == "dark") then
