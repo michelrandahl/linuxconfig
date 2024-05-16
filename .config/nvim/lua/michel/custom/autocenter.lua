897ebad08934e56b394d2c5c3918afa5e88651bb
michel <michel@randahl.net>
2024-05-16 16:38:22 +0200

    neovim: improve autocenter toggle

--- .config/nvim/lua/michel/custom/autocenter.lua
+++ .config/nvim/lua/michel/custom/autocenter.lua
@@ -3,22 +3,13 @@ function ToggleAutoCenter()
     if vim.b.auto_center_enabled then
         -- Auto-centering is currently enabled, so disable it
         -- Clear the autocmds for this buffer
-        vim.api.nvim_exec([[
-        augroup AutoCenter
-            autocmd! * <buffer>
-        augroup END
-        ]], false)
+        vim.opt_local.scrolloff = 0
         -- Set the buffer-local variable to false to indicate auto-centering is disabled
         vim.b.auto_center_enabled = false
     else
         -- Auto-centering is currently disabled, so enable it
         -- Define the autocmds for this buffer
-        vim.api.nvim_exec([[
-        augroup AutoCenter
-            autocmd! * <buffer>
-            autocmd CursorMoved <buffer> normal zz
-        augroup END
-        ]], false)
+        vim.opt_local.scrolloff = 999
         -- Set the buffer-local variable to true to indicate auto-centering is enabled
         vim.b.auto_center_enabled = true
     end
